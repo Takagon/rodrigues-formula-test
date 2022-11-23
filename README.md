9085155f6e341822979b5eda165fc117e9e4461a
Takagon <takamishunsuke2github@gmail.com>
2022-11-23 15:37:10 +0900

    update readme

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 ロドリゲスの公式を使って任意の軸に円盤を作ってみた。
 ![Test](/rodrigues-test.png)
+
+##実行環境
+python 3.6
--- rodrigues.py
+++ rodrigues.py
@@ -6,10 +6,10 @@ p = np.array([0,1,0]) #回転させる元の点
 n = np.array([1,-1,1])  #回転軸ベクトル
 K = np.array([[0,-n[2],n[1]],[n[2],0,-n[0]],[-n[1],n[0],0]]) #回転軸ベクトルnとの外積に対応する歪対称行列
 
-element = 36
+element = 36 #大きい値にすると滑らかな円盤に近づく
 step = np.linspace(0,360,element)   #初項0[deg],末項360[deg]のelement[個]の等差配列
 
-x = np.zeros(element)   #
+x = np.zeros(element)
 y = np.zeros(element)   
 z = np.zeros(element)   
 
