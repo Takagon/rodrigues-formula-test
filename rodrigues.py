4f3101412447c69f9fc084fc5461e49cd4b05a22
Takagon <takamishunsuke2github@gmail.com>
2022-11-23 14:25:51 +0900

    add comment

--- rodrigues.py
+++ rodrigues.py
@@ -1,27 +1,28 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-p = np.array([0,1,0])
+p = np.array([0,1,0]) #回転させる元の点
 
-n = np.array([1,-1,1])
-K = np.array([[0,-n[2],n[1]],[n[2],0,-n[0]],[-n[1],n[0],0]])
+n = np.array([1,-1,1])  #回転軸ベクトル
+K = np.array([[0,-n[2],n[1]],[n[2],0,-n[0]],[-n[1],n[0],0]]) #回転軸ベクトルnとの外積に対応する歪対称行列
 
 element = 36
-step = np.linspace(0,360,element)
+step = np.linspace(0,360,element)   #初項0[deg],末項360[deg]のelement[個]の等差配列
 
-x = np.zeros(element)
-y = np.zeros(element)
-z = np.zeros(element)
+x = np.zeros(element)   #
+y = np.zeros(element)   
+z = np.zeros(element)   
 
 count = 0
 for theta in step:
-    R = np.eye(3) + np.sin(np.deg2rad(theta))*K + (1-np.cos(np.deg2rad(theta)))*np.dot(K,K)
-    m_p = np.dot(p,R)
+    R = np.eye(3) + np.sin(np.deg2rad(theta))*K + (1-np.cos(np.deg2rad(theta)))*np.dot(K,K) #ロドリゲスの公式
+    m_p = np.dot(p,R) #点にを回転行列をかけて回転
     x[count] = m_p[0]
     y[count] = m_p[1]
     z[count] = m_p[2]
     count+=1
 
+#3次元散布図プロット
 fig = plt.figure()
 ax = fig.add_subplot(projection='3d')
 ax.scatter(x,y,z)
